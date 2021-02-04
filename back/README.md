ca42e7b465ed7baccccd0263dc0107b6d24ec627
cgodinho <cezar.godinho@hotmail.com>
2021-02-03 23:52:05 -0300

    correcoes

---
+++ back/README.md
@@ -0,0 +1,3 @@
+### `npm run dev`
+
+**Se der erro ao rodar o comando na primeira vez, vc dematar o processo, `crtl+c`, e rodar o comando novamente.**
--- back/dist/controllers/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.dataController = void 0;
-var data_1 = require("./Data/data");
-var dataController = new data_1.DataController();
-exports.dataController = dataController;
